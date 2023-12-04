f3c8c02d780e34f77f4519c523c483f03b57d896
David <davidcamps2000@gmail.com>
2023-12-04 18:37:07 +0100

    lop

---
+++ iniciosesion/project.js
@@ -0,0 +1,35 @@
+document.getElementById("projectForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let projectName = document.getElementById("projectName").value;
+
+    let maxMembers = document.getElementById("maxMembers").value
+
+    let sector = document.getElementById("sector").options[document.getElementById("sector").selectedIndex].text;
+
+    let demand = document.getElementById("demand").options[document.getElementById("demand").selectedIndex].text;
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "name": projectName,
+        "numMembers": parseInt(maxMembers)
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:8080/projects", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+    
+    
+})
