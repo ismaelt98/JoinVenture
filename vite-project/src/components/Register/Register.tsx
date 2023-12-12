00d900ce763e91e0e76b86f80eb9d9d2dce83e10
davidcamps123 <davidcamps2000@gmail.com>
2023-12-12 22:56:24 +0100

    anar fent

--- vite-project/src/components/Register/Register.tsx
+++ vite-project/src/components/Register/Register.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import style from './register.module.css'
-import { useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import { useNavigate } from 'react-router-dom';
+//import { ToastContainer, toast } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
 
 // function callRegisterEndPoint() {
 //   var myHeaders = new Headers();
@@ -14,8 +14,9 @@ import 'react-toastify/dist/ReactToastify.css';
 //     "email": "test@gmailo.cm",
 //     "password": "a197f08204c0b0db8dc9aad1c3ea1971a041fbbcf0158a875ced74536d987585",
 //     "phone": "121212",
-//     "roleuser": "admin"
+//     "roleuser": "PROGRAMADOR"
 //   });
+
 
 //   var requestOptions: RequestInit = {
 //     method: 'PUT',
@@ -49,7 +50,7 @@ function Register(): any {
     phone: '',
     roleuser: ''
   });
-  
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setFormData(prevState => ({
@@ -57,11 +58,34 @@ function Register(): any {
       [name]: value,
     }));
   };
-  
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     // Handle form submission with formData
-    alert(formData.username);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "username": formData.username,
+      "alias": formData.alias,
+      "email": formData.email,
+      "password": formData.password,
+      "phone": formData.phone,
+      "roleuser": "PROGRAMADOR"
+    });
+
+    var requestOptions: RequestInit = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("http://localhost:8080/users", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
     // You can send formData to an API endpoint, update state, etc.
   };
 
@@ -69,7 +93,7 @@ function Register(): any {
     <>
       <form className={style.signupForm} onSubmit={handleSubmit}>
         <div>
-          dshfjkhdsjkhsjkafd
+
           <label>
             Username:
             <input
@@ -77,9 +101,51 @@ function Register(): any {
               name="username"
               value={formData.username}
               onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Alias:
+            <input
+              type="text"
+              name="alias"
+              value={formData.alias}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Phone:
+            <input
+              type="number"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              required
             />
           </label>
         </div>
+        <button type="submit">Crear Cuenta</button>
       </form>
     </>
   );
