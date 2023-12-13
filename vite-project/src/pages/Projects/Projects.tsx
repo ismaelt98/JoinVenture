949907d4957f1caa5c3c5f4bd5c25dbc02eee3c2
David <davidcamps2000@gmail.com>
2023-12-14 00:12:00 +0100

    admin cosas

--- vite-project/src/pages/Projects/Projects.tsx
+++ vite-project/src/pages/Projects/Projects.tsx
@@ -1,26 +1,10 @@
 
-import { useEffect, useState } from 'react';
-import style from './projects.module.css'
-import Cookies from 'js-cookie';
+import style from './projects.module.css';
 import AllProjects from '../../components/AllProjects/AllProjects';
-function Projects(): any {
-  const [dataRoleUser, setDataRoleUser] = useState(true);
-  const roleuser = Cookies.get('roleuser');
 
-  useEffect(() => {
-
-    if (roleuser === 'programador') {
-      setDataRoleUser(true);
-    } else {
-      setDataRoleUser(false);
-    }
-
-  }, [roleuser]);
-
-
+function Projects(): any {  
   return (
-    <div className={style.container}>
- 
+    <div className={style.container}> 
       <AllProjects />
     </div>
   );
